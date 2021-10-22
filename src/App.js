e472e4f06d301901f177225abcc1578ad0f0f6db
FranMedic <m.francisca.medico@gmail.com>
2021-10-23 00:07:38 +0200

    added basic bootstrap to html and responsabilities

--- src/App.js
+++ src/App.js
@@ -1,24 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container ">
+        <div className="row justify-content-md-center">
+          <form className="col-lg-3">
+            <div className="form-group">
+              <label htmlFor="formName">Name</label>
+              <input
+                type="text"
+                className="form-control"
+                id="formName"
+                placeholder="Name"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="formLastName">Last Name</label>
+              <input
+                type="text"
+                className="form-control"
+                id="formLastName"
+                placeholder="Last Name"
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="formDate">Date</label>
+              <input
+                type="date"
+                className="form-control"
+                id="formDate"
+                placeholder="date"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="formYears">Years</label>
+              <input
+                type="text"
+                className="form-control"
+                id="formLastName"
+                placeholder="userYears"
+                readOnly
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="Email1">Email address</label>
+              <input
+                type="email"
+                class="form-control"
+                id="Email1"
+                aria-describedby="emailHelp"
+                placeholder="Enter email"
+              ></input>
+            </div>
+          </form>
+          <div className="row">
+            <form className="col-lg-3">
+              <div className="form-group">
+                <label htmlFor="InputPassword">Password</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="InputPassword"
+                  placeholder="Password"
+                />
+              </div>
+              <div className="form-group form-check">
+                <input
+                  type="checkbox"
+                  className="form-check-input"
+                  id="rememberCheck"
+                />
+                <label className="form-check-label" htmlFor="rememberCheck">
+                  Remember Me
+                </label>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
