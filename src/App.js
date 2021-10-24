81a4a11be6d7e720b5f813f32a1b43f80f29330a
FranMedic <m.francisca.medico@gmail.com>
2021-10-24 20:18:04 +0200

    added paginationData to call the other components depending the page number

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import Context from "./Components/Context/Context";
+import PaginationData from "./Components/PaginationData/PaginationData";
 
 function App() {
   const initialData = {
@@ -13,14 +14,42 @@ function App() {
   };
   const [userData, setUserData] = useState(initialData);
 
+  const changeData = (event) => {
+    setUserData({
+      ...userData,
+      [event.target.id]: event.target.value,
+    });
+  };
+
+  const [page, setPage] = useState(0);
+
+  const nextPage = () => {
+    if (page >= 0 && page <= 1) {
+      setPage(page + 1);
+    } else {
+      console.log(userData);
+    }
+  };
+
+  const prevPage = () => {
+    if (page >= 0 && page <= 2) {
+      setPage(page - 1);
+    }
+  };
   return (
     <Context.Provider
       value={{
         userData,
         setUserData,
+        changeData,
+        page,
+        nextPage,
+        prevPage,
       }}
     >
-      <section className="container d-flex justify-content-center align-items-center mh-100"></section>
+      <section className="container d-flex justify-content-center align-items-center mh-100">
+        <PaginationData />
+      </section>
     </Context.Provider>
   );
 }
